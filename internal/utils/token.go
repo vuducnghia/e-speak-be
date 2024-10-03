a67123ac64f2a0454275dfaedab1823fa2d4e60c
Bac Nguyen <nxbac.dev@gmail.com>
2024-10-03 16:50:07 +0700

    feat(jwt): verify token method

--- internal/utils/token.go
+++ internal/utils/token.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"errors"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -10,15 +11,42 @@ var secretKey = "VerySecretKey"
 
 type TokenPayload struct {
 	SessionId string `json:"session_id"`
+	*jwt.RegisteredClaims
 }
 
-func CreateToken(payload *TokenPayload, duration time.Duration) (string, error) {
-	claims := jwt.MapClaims{
-		"exp":     time.Now().Add(duration),
-		"payload": payload,
+func CreateToken(sessionId string, duration time.Duration) (string, error) {
+	payload := &TokenPayload{
+		SessionId: sessionId,
+		RegisteredClaims: &jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(duration)),
+		},
 	}
-	jwtToken := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	jwtToken := jwt.NewWithClaims(jwt.SigningMethodHS256, payload)
 	token, err := jwtToken.SignedString([]byte(secretKey))
 
 	return token, err
+}
+
+func VerifyToken(token string) (string, error) {
+	keyFunc := func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return "", errors.New("unexpected token signing method")
+		}
+		return secretKey, nil
+	}
+	jwtToken, err := jwt.Parse(token, keyFunc)
+	if err != nil {
+		return "", err
+	}
+	if !jwtToken.Valid {
+		return "", errors.New("invalid token")
+	}
+
+	payload, ok := jwtToken.Claims.(*TokenPayload)
+	if !ok {
+		return "", errors.New("invalid key type used as authorization key")
+	}
+
+	return payload.SessionId, nil
 }
