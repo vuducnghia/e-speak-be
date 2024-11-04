35c9789e24d6880df1b56c136be8a3f833227b8f
Bac Nguyen <nxbac.dev@gmail.com>
2024-11-04 19:48:27 +0700

    refactor(sql index): add index for vocabularies table

--- services/migrate/migrations/20241003145955_vocabularies_table.up.sql
+++ services/migrate/migrations/20241003145955_vocabularies_table.up.sql
@@ -1,8 +1,8 @@
 CREATE TABLE vocabularies (
     id varchar(255) PRIMARY KEY,
-    text varchar(255),
+    text varchar(255) NOT NULL UNIQUE,
     translation varchar(50),
     transcript_ipa varchar(255),
     audio_url varchar(255),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 );
